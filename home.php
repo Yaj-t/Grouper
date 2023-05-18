731299b507d0e26fcf83dc404e52b11cb165255f
T-jay Abunales <tjabunales@gmail.com>
2023-05-18 19:07:11 +0800

    added homepage

---
+++ home.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="styles.css" rel="stylesheet">
+</head>
+<body>
+    <div id="header">
+        <div id="nav">
+            <div id="events" class="navBar">
+                <a class="glow">Event List</a>
+                &nbsp;
+                <a class="glow">Plan Event</a>
+            </div>
+            &nbsp;
+            <div id="info" class="navBar">
+                <a class="glow">FAQ</a>
+                &nbsp;
+                <a class="glow">About</a>
+            </div>
+            &nbsp;
+            <div id="login" class="navBar">
+                <a class="glow">Log In</a>
+                &nbsp;
+                <a class="glow">Sign In</a>
+            </div>
+        </div>
+    </div>
+    <div id="body">
+        <p id="title" class="glow">Groupr</p>
+        <form method="post" action="">
+            <input id="search" placeholder="Search for events" name="event" type="text">
+            <input type="submit" class="button">
+        </form>
+    </div>
+</body>
+</html>
